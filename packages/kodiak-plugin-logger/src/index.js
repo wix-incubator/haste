16b5ac32252a6f3c1cbdfe23ac167bc504e58a5d
Ronen Amiel <ronen.amiel@gmail.com>
2017-09-16 13:33:07 +0300

    fix kodiak-task-logger with api changes

--- packages/kodiak-plugin-logger/src/index.js
+++ packages/kodiak-plugin-logger/src/index.js
@@ -1,4 +1,3 @@
-const path = require('path');
 const chalk = require('chalk');
 const { format, delta } = require('./utils');
 
@@ -8,16 +7,16 @@ module.exports = class LoggerPlugin {
       ['stdout', 'stderr'].forEach(name => task.child[name].pipe(process[name]));
 
       const start = new Date();
-      console.log(`[${format(start)}] ${chalk.black.bgGreen('Starting')} '${path.basename(task.module)}'...`);
+      console.log(`[${format(start)}] ${chalk.black.bgGreen('Starting')} '${task.modulePath}'...`);
 
       task.plugin('succeed-task', () => {
         const [end, time] = delta(start);
-        console.log(`[${format(end)}] ${chalk.black.bgCyan('Finished')} '${path.basename(task.module)}' after ${time} ms`);
+        console.log(`[${format(end)}] ${chalk.black.bgCyan('Finished')} '${task.modulePath}' after ${time} ms`);
       });
 
       task.plugin('failed-task', () => {
         const [end, time] = delta(start);
-        console.log(`[${format(end)}] ${chalk.white.bgRed('Failed')} '${path.basename(task.module)}' after ${time} ms`);
+        console.log(`[${format(end)}] ${chalk.white.bgRed('Failed')} '${task.modulePath}' after ${time} ms`);
       });
     });
   }
