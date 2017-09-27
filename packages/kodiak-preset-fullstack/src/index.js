9f7ab2f94984d71aef422aac0202ef60791bc8ae
Ronen Amiel <ronen.amiel@gmail.com>
2017-09-27 19:57:44 +0300

    [wip] naive preset to start working with

--- packages/kodiak-preset-fullstack/src/index.js
+++ packages/kodiak-preset-fullstack/src/index.js
@@ -7,6 +7,49 @@ const paths = {
 };
 
 module.exports.build = async (configure) => {
+  const { define } = configure({
+    plugins: [
+      new LoggerPlugin(),
+    ],
+  });
+
+  const clean = define({ name: 'clean' });
+  const babel = define({ name: 'babel' });
+  const sass = define({ name: 'sass' });
+  const copy = define({ name: 'copy' });
+  const webpack = define({ name: 'webpack' });
+
+  await clean({ pattern: `{${paths.build},${paths.target}}/*` });
+
+  await Promise.all([
+    sass({ pattern: paths.javascripts, output: paths.build }),
+    copy({ pattern: paths.javascripts, output: paths.build }),
+    babel({ pattern: paths.javascripts, output: paths.build }),
+    webpack({ pattern: paths.javascripts, output: paths.build }),
+  ]);
+
+  return {
+    persistent: false,
+  };
+};
+
+module.exports.lint = async (configure) => {
+  const { define } = configure({
+    plugins: [
+      new LoggerPlugin(),
+    ],
+  });
+
+  const eslint = define({ name: 'eslint' });
+
+  await eslint({ pattern: paths.javascripts, output: paths.build });
+
+  return {
+    persistent: false,
+  };
+};
+
+module.exports.start = async (configure) => {
   const { define, watch } = configure({
     plugins: [
       new LoggerPlugin(),
@@ -14,22 +57,54 @@ module.exports.build = async (configure) => {
   });
 
   const clean = define({ name: 'clean' });
-
   const babel = define({ name: 'babel' });
+  const sass = define({ name: 'sass' });
+  const copy = define({ name: 'copy' });
+  const webpack = define({ name: 'webpack' });
+  const server = define({ name: 'server' });
+
+  await clean({ pattern: `{${paths.build},${paths.target}}/*` });
 
   await Promise.all([
-    clean({ pattern: `{${paths.build},${paths.target}}/*` }),
-  ]);
-
-  await Promise.all([
+    sass({ pattern: paths.javascripts, output: paths.build }),
+    copy({ pattern: paths.assets, output: paths.build }),
     babel({ pattern: paths.javascripts, output: paths.build }),
+    webpack({ pattern: paths.javascripts, output: paths.build, watch }),
   ]);
+
+  const restart = await server({ file });
 
   watch(paths.javascripts, async (changed) => {
     await babel({ pattern: changed, output: paths.build });
+    await restart();
+  });
+
+  watch(paths.styles, async (changed) => {
+    await sass({ pattern: changed, output: paths.build });
+  });
+
+  watch(paths.assets, async (changed) => {
+    await copy({ pattern: changed, output: paths.build });
   });
 
   return {
     persistent: true,
   };
 };
+
+module.exports.test = async (configure, { watch }) => {
+  const { define } = configure({
+    plugins: [
+      new LoggerPlugin(),
+    ],
+  });
+
+  const jest = define({ name: 'jest' });
+
+  await jest({ pattern: paths.test, watch });
+
+  return {
+    persistent: watch,
+  };
+};
+
