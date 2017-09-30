e89b76c58d6f2dbed30c2764272af823180706de
Ronen Amiel <ronen.amiel@gmail.com>
2017-09-30 19:01:11 +0300

    haste-task-babel: tests

--- packages/haste-task-babel/src/index.js
+++ packages/haste-task-babel/src/index.js
@@ -1,8 +1,8 @@
 const { transform } = require('babel-core');
 
-module.exports = () => async (files) => {
+module.exports = options => async (files) => {
   return files.map(({ filename, content }) => {
-    const { code, map } = transform(content, { filename });
+    const { code, map } = transform(content, Object.assign({ filename }, options));
 
     return {
       filename,
