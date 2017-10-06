51d2e6e07003b4d33a2b067205480cdf6d5105e0
Ronen Amiel <ronen.amiel@gmail.com>
2017-10-06 15:50:25 +0300

    have a general solution for logging errors

--- packages/haste-task-babel/src/index.js
+++ packages/haste-task-babel/src/index.js
@@ -2,19 +2,14 @@ const { transform } = require('babel-core');
 
 module.exports = options => async (files) => {
   return files.map(({ filename, content }) => {
-    try {
-      const { code, map } = transform(content, Object.assign({ filename }, options));
+    const { code, map } = transform(content, Object.assign({ filename }, options));
 
-      console.log('compiled - ', filename);
+    console.log('compiled - ', filename);
 
-      return {
-        filename,
-        content: code,
-        map
-      };
-    } catch (e) {
-      console.error(e);
-      throw e;
-    }
+    return {
+      filename,
+      content: code,
+      map
+    };
   });
 };
