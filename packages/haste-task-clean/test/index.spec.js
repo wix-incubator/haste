49d3e17c5c788c206dee44afc9bfd20b8695114b
Ronen Amiel <ronen.amiel@gmail.com>
2017-10-09 23:26:18 +0300

    basic test suit for haste-task-clean

---
+++ packages/haste-task-clean/test/index.spec.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const tempWrite = require('temp-write');
+const clean = require('../src');
+
+describe('haste-clean', () => {
+  it('should remove all files and folders a file path', async () => {
+    const tempFilename = await tempWrite('unicorn', 'pony.png');
+
+    const task = clean({ pattern: tempFilename });
+
+    return task()
+      .then(() => {
+        expect(fs.existsSync(tempFilename)).toEqual(false);
+      });
+  });
+
+  it('should remove all files and folders a from a glob pattern', async () => {
+    const tempFilename = await tempWrite('unicorn', 'pony.png');
+
+    const task = clean({ pattern: path.join(path.dirname(tempFilename), '**/*.*') });
+
+    return task()
+      .then(() => {
+        expect(fs.existsSync(tempFilename)).toEqual(false);
+      });
+  });
+});
