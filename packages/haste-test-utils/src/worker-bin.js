119b16115cfc0b2f919105f1473e2f32e16f8625
Ronen Amiel <ronen.amiel@gmail.com>
2017-10-22 16:24:39 +0300

    pass an empty object by default for tasks in haste-test-utils

--- packages/haste-test-utils/src/worker-bin.js
+++ packages/haste-test-utils/src/worker-bin.js
@@ -1,6 +1,6 @@
 const task = require(process.argv[2]);
 
-process.on('message', ({ options, input }) => {
+process.on('message', ({ options = {}, input }) => {
   task(options)(input)
     .then((result = {}) => process.send({ result }))
     .catch((error = {}) => process.send({ error }));
