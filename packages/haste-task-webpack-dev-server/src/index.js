7e585564271cfd4a6855d8c4bd651d9920b5b1a9
Ronen Amiel <ronena@wix.com>
2017-10-24 15:58:41 +0200

    haste-task-webpack-dev-server: support passing decorator that accepts express instance (#96)

--- packages/haste-task-webpack-dev-server/src/index.js
+++ packages/haste-task-webpack-dev-server/src/index.js
@@ -2,13 +2,18 @@ const webpack = require('webpack');
 const express = require('express');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 
-module.exports = ({ configPath, port = 9200, hostname = 'localhost' }) => () => {
+module.exports = ({ configPath, decoratorPath, port = 9200, hostname = 'localhost' }) => () => {
   return new Promise((resolve) => {
     const app = express();
     const config = require(configPath);
     const compiler = webpack(config);
 
     app.use(webpackDevMiddleware(compiler));
+
+    if (decoratorPath) {
+      require(decoratorPath)(app);
+    }
+
     app.listen(port, hostname, resolve);
   });
 };
