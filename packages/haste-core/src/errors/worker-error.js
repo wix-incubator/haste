71305227bafb328fb1213d4a7beb334eeceec231
Ran Yitzhaki <ranyitzhaki@gmail.com>
2017-10-24 20:14:10 +0300

    handle a case where a task is rejected with undefined

--- packages/haste-core/src/errors/worker-error.js
+++ packages/haste-core/src/errors/worker-error.js
@@ -3,7 +3,9 @@ module.exports = class extends Error {
     super();
 
     this.name = 'WorkerError';
-    this.message = `Error in worker: ${error.stack}`;
+    this.message = error ?
+      `Error in worker: ${error.stack}` :
+      'Unknown error in worker';
     this.error = error;
   }
 };
