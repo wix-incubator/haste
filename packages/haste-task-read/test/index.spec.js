3b841a32aec6d5ddb947a418996f85b72251ed80
Ronen Amiel <ronena@wix.com>
2017-10-26 13:57:24 +0200

    haste-task-read: should read only files and not directories (#98)

--- packages/haste-task-read/test/index.spec.js
+++ packages/haste-task-read/test/index.spec.js
@@ -4,7 +4,8 @@ const read = require('../src');
 
 describe('haste-read', () => {
   const resolve = filename => path.join(__dirname, 'fixtures', filename);
-  const filename = resolve('some-file.txt');
+  const filename = resolve('file.txt');
+  const nestedFilename = resolve('nested/folder/structure/nested.txt');
 
   it('should read from a filename', () => {
     const task = read({ pattern: filename });
@@ -21,12 +22,27 @@ describe('haste-read', () => {
   });
 
   it('should read from a glob pattern', () => {
-    const pattern = resolve('**/*.*');
+    const pattern = resolve('**.*');
     const task = read({ pattern });
 
     const expected = {
       filename,
       content: fs.readFileSync(filename, 'utf8'),
+    };
+
+    return task()
+      .then((result) => {
+        expect(result).toEqual([expected]);
+      });
+  });
+
+  it('should not read directories', () => {
+    const pattern = resolve('nested/**');
+    const task = read({ pattern });
+
+    const expected = {
+      filename: nestedFilename,
+      content: fs.readFileSync(nestedFilename, 'utf8'),
     };
 
     return task()
