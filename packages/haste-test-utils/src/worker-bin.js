ec6b408a7a7e64fa274dc305cf15a969569168c7
Ronen Amiel <ronen.amiel@gmail.com>
2017-11-02 12:04:50 +0200

    haste-test-utils: log an error if a worker fails

--- packages/haste-test-utils/src/worker-bin.js
+++ packages/haste-test-utils/src/worker-bin.js
@@ -2,6 +2,12 @@ const task = require(process.argv[2]);
 
 process.on('message', ({ options = {}, input, id }) => {
   task(options)(input)
-    .then((result = {}) => process.send({ result, id }))
-    .catch((error = {}) => process.send({ error, id }));
+    .then(result => process.send({ id, result, type: 'success' }))
+    .catch((error) => {
+      if (error) {
+        console.log(error);
+      }
+
+      process.send({ id, error, type: 'failure' });
+    });
 });
