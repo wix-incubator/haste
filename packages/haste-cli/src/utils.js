8216901c9341de20e3bec9b74c71c606101f6006
Yarden Sod-Moriah <yardnsm@gmail.com>
2017-11-09 09:31:00 +0200

    Remove resolvePresetName

--- packages/haste-cli/src/utils.js
+++ packages/haste-cli/src/utils.js
@@ -7,11 +7,3 @@ const standardizePresetName = module.exports.standardizePresetName = (name) => {
   // example -> haste-preset-example
   return name.replace(HASTE_PRESET_PREFIX_REGEX, 'haste-preset-');
 };
-
-module.exports.resolvePresetName = (presetName, runContext) => {
-  if (path.isAbsolute(presetName)) {
-    return presetName;
-  }
-
-  return resolveFrom(runContext, standardizePresetName(presetName));
-};
