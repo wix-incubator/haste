701a3ee904ae0c1112dc795453049f2eac3f8cdd
Yarden Sod-Moriah <yardnsm@gmail.com>
2017-11-09 09:31:43 +0200

    Fix lint

--- packages/haste-cli/src/utils.js
+++ packages/haste-cli/src/utils.js
@@ -1,9 +1,6 @@
-const path = require('path');
-const resolveFrom = require('resolve-from');
-
 const HASTE_PRESET_PREFIX_REGEX = /^(?!@|[^/]+\/|haste-preset-)/;
 
-const standardizePresetName = module.exports.standardizePresetName = (name) => {
+module.exports.standardizePresetName = (name) => {
   // example -> haste-preset-example
   return name.replace(HASTE_PRESET_PREFIX_REGEX, 'haste-preset-');
 };
