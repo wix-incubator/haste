30ab45625f3b3dbcdbaa204bfca0dac90b932551
Ran Yitzhaki <ranyitzhaki@gmail.com>
2017-11-10 01:29:11 +0200

    haset-cli: don't normalize absolute preset names

--- packages/haste-cli/src/utils.js
+++ packages/haste-cli/src/utils.js
@@ -1,6 +1,16 @@
+const path = require('path');
+
 const HASTE_PRESET_PREFIX_REGEX = /^(?!@|[^/]+\/|haste-preset-)/;
 
 module.exports.standardizePresetName = (name) => {
   // example -> haste-preset-example
   return name.replace(HASTE_PRESET_PREFIX_REGEX, 'haste-preset-');
+};
+
+module.exports.resolvePresetName = (name) => {
+  if (path.isAbsolute(name)) {
+    return name;
+  }
+
+  return module.exports.standardizePresetName(name);
 };
