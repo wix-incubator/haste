3f0fc1ff2bad25d89a94e6aee6cb67663f7b2f32
Ronen Amiel <ronen.amiel@gmail.com>
2017-11-15 18:44:55 +0200

    haste-task-webpack: slightly refactor tests by nesting tests under describes

--- packages/haste-task-webpack/test/index.spec.js
+++ packages/haste-task-webpack/test/index.spec.js
@@ -10,26 +10,54 @@ const configFunctionPath = require.resolve('./fixtures/webpack.config.function')
 const callbackPath = require.resolve('./fixtures/callback');
 
 describe('haste-webpack', () => {
-  it('should bundle with webpack', async () => {
-    const task = webpack({ configPath });
+  describe('when configPath is a path to an object', () => {
+    it('should bundle with webpack', async () => {
+      const task = webpack({ configPath });
 
-    await task();
-
-    const bundlePath = path.join(config.output.path, config.output.filename);
-    expect(fs.existsSync(bundlePath)).toEqual(true);
-  });
-
-  it('should reject if webpack fails', async () => {
-    expect.assertions(1);
-
-    const task = webpack({
-      configPath: require.resolve('./fixtures/webpack.config.invalid'),
-    });
-    try {
       await task();
-    } catch (error) {
-      expect(error.message).toMatch(/Invalid configuration object/);
-    }
+
+      const bundlePath = path.join(config.output.path, config.output.filename);
+      expect(fs.existsSync(bundlePath)).toEqual(true);
+    });
+
+    it('should reject if webpack fails', async () => {
+      expect.assertions(1);
+
+      const task = webpack({
+        configPath: require.resolve('./fixtures/webpack.config.invalid'),
+      });
+      try {
+        await task();
+      } catch (error) {
+        expect(error.message).toMatch(/Invalid configuration object/);
+      }
+    });
+
+    it('should support passing callback that accepts webpack err and stats', async () => {
+      const { command, kill } = run(require.resolve('../src'));
+      const { task, stdout } = command({ configPath, callbackPath });
+
+      try {
+        await task();
+        expect(stdout()).toMatch('1 module');
+      } finally {
+        kill();
+      }
+    });
+
+    it('should reject if there are compilation erros', async () => {
+      expect.assertions(1);
+
+      const task = webpack({
+        configPath: require.resolve('./fixtures/webpack.config.error'),
+      });
+
+      try {
+        await task();
+      } catch (error) {
+        expect(error).toMatch('Module not found');
+      }
+    });
   });
 
   describe('when configPath is a path to a function', () => {
@@ -49,31 +77,5 @@ describe('haste-webpack', () => {
       const bundlePath = path.join(configParams.output.path, configParams.output.filename);
       expect(fs.existsSync(bundlePath)).toEqual(true);
     });
-  });
-
-  it('should support passing callback that accepts webpack err and stats', async () => {
-    const { command, kill } = run(require.resolve('../src'));
-    const { task, stdout } = command({ configPath, callbackPath });
-
-    try {
-      await task();
-      expect(stdout()).toMatch('1 module');
-    } finally {
-      kill();
-    }
-  });
-
-  it('should reject if there are compilation erros', async () => {
-    expect.assertions(1);
-
-    const task = webpack({
-      configPath: require.resolve('./fixtures/webpack.config.error'),
-    });
-
-    try {
-      await task();
-    } catch (error) {
-      expect(error).toMatch('Module not found');
-    }
   });
 });
