581679673c53c6bd042f8e965c76f4589b677bac
Ronen Amiel <ronen.amiel@gmail.com>
2017-11-25 12:40:22 +0200

    haste-cli: keep bin file small, move most logic to /src

---
+++ packages/haste-cli/src/index.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const yargs = require('yargs');
+const resolveFrom = require('resolve-from');
+const get = require('lodash/get');
+const { resolvePresetName, loadConfig } = require('../src/utils');
+
+module.exports = async () => {
+  const { argv } = yargs
+    .option('p', {
+      alias: 'preset',
+      demandOption: false,
+      describe: 'The full name of the preset node module ([haste-preset-name]) or an absolute path to a preset',
+      type: 'string'
+    })
+    .version()
+    .help();
+
+  const [command] = argv._;
+
+  if (!command) {
+    console.error('You must specify a command for Haste to run');
+    process.exit(1);
+  }
+
+  const appDirectory = fs.realpathSync(process.cwd());
+
+  const config = loadConfig(appDirectory);
+  const presetName = argv.preset || get(config, 'preset');
+
+  if (!presetName) {
+    console.error('You must pass a preset through cli option \'--preset\', .hasterc, or package.json configs');
+    process.exit(1);
+  }
+
+  const presetPath = resolveFrom.silent(appDirectory, resolvePresetName(presetName));
+
+  if (!presetPath) {
+    console.error(`Unable to find ${presetPath}, please make sure it is installed`);
+    process.exit(1);
+  }
+
+  const preset = require(presetPath);
+  const action = preset[command];
+
+  if (!action) {
+    console.error(`${presetName} doesn't support command '${command}'`);
+    process.exit(1);
+  }
+
+  try {
+    const { persistent } = await action({ context: presetPath });
+
+    if (!persistent) {
+      process.exit(0);
+    }
+  } catch (error) {
+    if (error.name !== 'WorkerError') {
+      console.error(error);
+    }
+
+    process.exit(1);
+  }
+};
