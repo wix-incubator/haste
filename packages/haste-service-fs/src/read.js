c0c5d7a2e33697d92127755a259b0c9f6e668e2a
Ronen Amiel <ronen.amiel@gmail.com>
2017-11-25 19:17:42 +0200

    haste-service-fs: initial service with read/write transported from read/write tasks

---
+++ packages/haste-service-fs/src/read.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const globby = require('globby');
+const { readFile } = require('./utils');
+
+module.exports = async ({ pattern, options: { cwd = process.cwd(), ...options } = {} }) => {
+  const defaultOptions = { nodir: true };
+
+  const files = await globby(pattern, {
+    ...defaultOptions,
+    ...options,
+    cwd,
+  });
+
+  return Promise.all(
+    files
+      .map((filename) => {
+        const absoluteFilePath = path.isAbsolute(filename) ? filename : path.join(cwd, filename);
+
+        return readFile(absoluteFilePath)
+          .then((content) => {
+            return {
+              content,
+              filename,
+              cwd,
+            };
+          });
+      })
+  );
+};
---
+++ packages/haste-service-fs/src/utils.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+const mkdirp = require('mkdirp');
+
+const makeDir = name => new Promise((resolve, reject) =>
+  mkdirp(name, err => err ? reject(err) : resolve())
+);
+
+module.exports.writeFile = (filename, content) => new Promise(async (resolve, reject) => {
+  await makeDir(path.dirname(filename));
+  fs.writeFile(filename, content, 'utf8', err => err ? reject(err) : resolve());
+});
+
+module.exports.readFile = file => new Promise((resolve, reject) => {
+  fs.readFile(file, 'utf8', (err, data) => err ? reject(err) : resolve(data));
+});
