7c948868ef0adeed2efbb09c909456ab4bd0a5e1
Ronen Amiel <ronen.amiel@gmail.com>
2017-11-29 00:04:52 +0200

    haste-test-utils: use an empty object by default for setup()

--- packages/haste-test-utils/src/fs-setup.js
+++ packages/haste-test-utils/src/fs-setup.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs-extra');
 const tempy = require('tempy');
 
-module.exports = (fsObject) => {
+module.exports = (fsObject = {}) => {
   const cwd = tempy.directory();
 
   Object.keys(fsObject).forEach((filename) => {
