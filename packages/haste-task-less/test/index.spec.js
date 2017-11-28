9de5de24995f1faac5bf7e140eeb1e95b748b4d6
Ronen Amiel <ronen.amiel@gmail.com>
2017-11-29 00:46:53 +0200

    haste-task-less: adjust to v0.2.0 api changes

--- packages/haste-task-less/test/index.spec.js
+++ packages/haste-task-less/test/index.spec.js
@@ -1,88 +1,91 @@
 const fs = require('fs');
 const path = require('path');
-const less = require('../src');
+const { setup } = require('haste-test-utils');
+
+const taskPath = require.resolve('../src');
+
+const fromFixture = (filename) => {
+  return fs.readFileSync(path.join(__dirname, filename), 'utf8');
+};
 
 describe('haste-less', () => {
-  it('should transpile less to css', () => {
-    const task = less();
+  it('should transpile less to css', async () => {
+    const { run, files } = await setup({
+      'test.less': fromFixture('fixtures/test.less'),
+    });
 
-    const file = {
-      filename: 'test.less',
-      content: fs.readFileSync(path.join(__dirname, 'fixtures/test.less'), 'utf8'),
-    };
-
-    const expected = {
-      filename: 'test.less',
-      content: fs.readFileSync(path.join(__dirname, 'expected/test.css'), 'utf8'),
-      map: undefined,
-    };
-
-    return task([file])
-      .then((result) => {
-        expect(result).toEqual([expected]);
+    await run(async ({ [taskPath]: less }) => {
+      await less({
+        pattern: '*.less',
+        target: 'dist',
       });
+    });
+
+    const expected = fromFixture('expected/test.css');
+
+    expect(files['dist/test.less']).toMatch(expected);
   });
 
-  it('should fail for invalid less', () => {
+  it('should fail for invalid less', async () => {
     expect.assertions(1);
 
-    const task = less();
+    const { run } = await setup({
+      'invalid.less': fromFixture('fixtures/invalid.less'),
+    });
 
-    const file = {
-      filename: 'invalid.less',
-      content: fs.readFileSync(path.join(__dirname, 'fixtures/invalid.less'), 'utf8'),
-    };
-
-    return task([file])
-      .catch((error) => {
-        expect(error.message).toEqual('Unrecognised input. Possibly missing something');
-      });
+    await run(async ({ [taskPath]: less }) => {
+      try {
+        await less({
+          pattern: '*.less',
+          target: 'dist',
+        });
+      } catch (error) {
+        expect(error.message).toMatch('Unrecognised input. Possibly missing something');
+      }
+    });
   });
 
-  it('should generate source maps', () => {
-    const task = less({ sourceMap: { outputSourceFiles: true } });
+  it('should generate source maps', async () => {
+    const { run, files } = await setup({
+      'test.less': fromFixture('fixtures/test.less'),
+    });
 
-    const file = {
-      filename: 'test.less',
-      content: fs.readFileSync(path.join(__dirname, 'fixtures/test.less'), 'utf8'),
-    };
-
-    const expected = {
-      filename: 'test.less',
-      content: fs.readFileSync(path.join(__dirname, 'expected/test.css'), 'utf8'),
-      map: {
-        version: 3,
-        sources: ['test.less'],
-        names: [],
-        mappings: 'AAGA;EACC,qBAAA;EACA,cAAA;;AAID;EACC,YAAA;EACA,WAAA;EACA,qBAAA',
-        sourcesContent: [
-          fs.readFileSync(path.join(__dirname, 'fixtures/test.less'), 'utf8')
-        ]
-      },
-    };
-
-    return task([file])
-      .then((result) => {
-        expect(result).toEqual([expected]);
+    await run(async ({ [taskPath]: less }) => {
+      await less({
+        pattern: '*.less',
+        target: 'dist',
+        options: { sourceMap: { outputSourceFiles: true } },
       });
+    });
+
+    const map = {
+      version: 3,
+      sources: ['test.less'],
+      names: [],
+      mappings: 'AAGA;EACC,qBAAA;EACA,cAAA;;AAID;EACC,YAAA;EACA,WAAA;EACA,qBAAA',
+      sourcesContent: [
+        fromFixture('fixtures/test.less')
+      ]
+    };
+
+    expect(JSON.parse(files['dist/test.less.map'])).toEqual(map);
   });
 
-  it('should handle includePaths', () => {
-    const task = less({ paths: [path.join(__dirname, 'fixtures')] });
+  it('should handle includePaths', async () => {
+    const { run, files } = await setup({
+      'includePaths.less': fromFixture('fixtures/includePaths.less'),
+    });
 
-    const file = {
-      filename: 'includePaths.less',
-      content: fs.readFileSync(path.join(__dirname, 'fixtures/includePaths.less'), 'utf8'),
-    };
-
-    const expected = {
-      filename: 'includePaths.less',
-      content: fs.readFileSync(path.join(__dirname, 'expected/includePaths.css'), 'utf8'),
-    };
-
-    return task([file])
-      .then((result) => {
-        expect(result).toEqual([expected]);
+    await run(async ({ [taskPath]: less }) => {
+      await less({
+        pattern: '*.less',
+        target: 'dist',
+        options: { paths: [path.join(__dirname, 'fixtures')] },
       });
+    });
+
+    const expected = fromFixture('expected/includePaths.css');
+
+    expect(files['dist/includePaths.less']).toMatch(expected);
   });
 });
