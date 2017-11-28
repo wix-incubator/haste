4e61909aa1cc3f1a916f245d365173c401c87d92
Ronen Amiel <ronen.amiel@gmail.com>
2017-11-28 22:31:45 +0200

    haste-worker-farm: fix error-parsing failing from using the wrong variable

--- packages/haste-worker-farm/src/utils.js
+++ packages/haste-worker-farm/src/utils.js
@@ -2,7 +2,7 @@ const WorkerError = require('./errors/worker-error');
 
 module.exports.parseError = (errorObj) => {
   const error = Object.getOwnPropertyNames(errorObj).reduce((obj, key) => {
-    return Object.assign(obj, { [key]: error[key] });
+    return Object.assign(obj, { [key]: errorObj[key] });
   });
 
   return new WorkerError(error);
