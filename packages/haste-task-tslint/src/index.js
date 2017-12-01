6ea82874cf50ada30792065531f136ae6d996b30
Ronen Amiel <ronen.amiel@gmail.com>
2017-12-01 15:26:05 +0200

    haste-task-tslint: adjust to v0.2.0 api changes

--- packages/haste-task-tslint/src/index.js
+++ packages/haste-task-tslint/src/index.js
@@ -11,14 +11,15 @@ function runLinter(options, configurationFilePath, files) {
   return linter.getResult();
 }
 
-module.exports = ({ options = { formatter: 'prose' }, configurationFilePath = null } = {}) => (files) => {
-  try {
-    const { errorCount, output } = runLinter(options, configurationFilePath, files);
+module.exports = async ({
+  pattern,
+  options = { formatter: 'prose' },
+  configurationFilePath = null,
+} = {}, { fs }) => {
+  const files = await fs.read({ pattern });
+  const { errorCount, output } = runLinter(options, configurationFilePath, files);
 
-    return errorCount > 0 ?
-      Promise.reject(output) :
-      Promise.resolve();
-  } catch (error) {
-    return Promise.reject(error);
+  if (errorCount > 0) {
+    throw output;
   }
 };
