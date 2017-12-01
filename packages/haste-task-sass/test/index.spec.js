cadbe37a1aa372373d0a98345f6a54c8f2681f5b
Ronen Amiel <ronen.amiel@gmail.com>
2017-12-01 15:11:40 +0200

    haste-task-sass: adjust to v0.2.0 api changes

--- packages/haste-task-sass/test/index.spec.js
+++ packages/haste-task-sass/test/index.spec.js
@@ -1,102 +1,123 @@
 const fs = require('fs');
 const path = require('path');
-const sass = require('../src');
+const { setup } = require('haste-test-utils');
+
+const taskPath = require.resolve('../src');
+
+const fromFixture = (filename) => {
+  return fs.readFileSync(path.join(__dirname, filename), 'utf8');
+};
 
 describe('haste-sass', () => {
-  it('should transpile scss to css', () => {
-    const task = sass();
+  it('should transpile scss to css', async () => {
+    const { run, files } = await setup({
+      'test.scss': fromFixture('fixtures/test.scss'),
+    });
 
-    const file = {
-      filename: 'test.scss',
-      content: fs.readFileSync(path.join(__dirname, 'fixtures/test.scss'), 'utf8'),
-    };
-
-    const expected = {
-      filename: 'test.scss',
-      content: fs.readFileSync(path.join(__dirname, 'expected/test.css'), 'utf8'),
-    };
-
-    return task([file])
-      .then((result) => {
-        expect(result).toEqual([expected]);
+    await run(async ({ [taskPath]: sass }) => {
+      await sass({
+        pattern: '*.scss',
+        target: 'dist',
       });
+    });
+
+    const expected = fromFixture('expected/test.css');
+
+    expect(files['dist/test.scss'].content).toMatch(expected);
   });
 
-  it('should fail for invalid scss', () => {
+  it('should fail for invalid scss', async () => {
     expect.assertions(1);
 
-    const task = sass();
+    const { run } = await setup({
+      'invalid.scss': fromFixture('fixtures/invalid.scss'),
+    });
 
-    const file = {
-      filename: 'invalid.scss',
-      content: fs.readFileSync(path.join(__dirname, 'fixtures/invalid.scss'), 'utf8'),
-    };
-
-    return task([file])
-      .catch((error) => {
-        expect(error.message).toEqual('Invalid CSS after "hello world": expected "{", was ""');
-      });
+    await run(async ({ [taskPath]: sass }) => {
+      try {
+        await sass({
+          pattern: '*.scss',
+          target: 'dist',
+        });
+      } catch (error) {
+        expect(error.message).toMatch('Invalid CSS after "hello world": expected "{", was ""');
+      }
+    });
   });
 
-  it('should handle includePaths', () => {
-    const task = sass({ includePaths: [path.join(__dirname, 'fixtures')] });
+  it('should handle includePaths', async () => {
+    const { run, files } = await setup({
+      'includePaths.scss': fromFixture('fixtures/includePaths.scss'),
+    });
 
-    const file = {
-      filename: 'test.scss',
-      content: fs.readFileSync(path.join(__dirname, 'fixtures/includePaths.scss'), 'utf8'),
-    };
-
-    const expected = {
-      filename: 'test.scss',
-      content: fs.readFileSync(path.join(__dirname, 'expected/includePaths.css'), 'utf8'),
-    };
-
-    return task([file])
-      .then((result) => {
-        expect(result).toEqual([expected]);
+    await run(async ({ [taskPath]: sass }) => {
+      await sass({
+        pattern: '*.scss',
+        target: 'dist',
+        options: { includePaths: [path.join(__dirname, 'fixtures')] },
       });
+    });
+
+    const expected = fromFixture('expected/includePaths.css');
+
+    expect(files['dist/includePaths.scss'].content).toMatch(expected);
   });
 
-  it('should ignore underscore partial files', () => {
-    const task = sass();
+  it('should ignore underscore partial files', async () => {
+    const { run, files } = await setup({
+      '_partial.scss': fromFixture('fixtures/test.scss'),
+    });
 
-    const file = {
-      filename: '_partial.scss',
-      content: fs.readFileSync(path.join(__dirname, 'fixtures/test.scss'), 'utf8'),
-    };
-
-    return task([file])
-      .then((result) => {
-        expect(result).toEqual([]);
+    await run(async ({ [taskPath]: sass }) => {
+      await sass({
+        pattern: '*.scss',
+        target: 'dist',
       });
+    });
+
+    expect(files['dist/_partial.scss'].exists).toEqual(false);
   });
 
-  it('should generate source maps', () => {
-    const task = sass({ sourceMap: true });
+  it('should generate source maps', async () => {
+    const { run, files } = await setup({
+      'test.scss': fromFixture('fixtures/test.scss'),
+    });
 
-    const file = {
-      filename: 'test.scss',
-      content: fs.readFileSync(path.join(__dirname, 'fixtures/test.scss'), 'utf8'),
+    await run(async ({ [taskPath]: sass }) => {
+      await sass({
+        pattern: '*.scss',
+        target: 'dist',
+        options: { sourceMap: true },
+      });
+    });
+
+    const map = {
+      version: 3,
+      file: 'test.scss',
+      sources: [
+        'test.scss'
+      ],
+      names: [],
+      mappings: 'AAGA,AAAA,mBAAmB,CAAC;EACnB,YAAY,EAJN,OAAO;EAKb,KAAK,EACJ,OAAiB,GAClB;;AAED,AAAA,OAAO,CAAC;EACP,OAAO,EAAE,GAAW;EACpB,MAAM,EAAE,GAAW;EACnB,YAAY,EAZN,OAAO,GAab'
     };
 
-    return task([file])
-      .then(([result]) => {
-        expect(result.content).toMatch(/\/\*# sourceMappingURL=test\.scss\.map/);
-        expect(result.map).toMatch(/test\.scss"/);
-      });
+    expect(files['dist/test.scss'].content).toMatch('/*# sourceMappingURL=test.scss.map');
+    expect(JSON.parse(files['dist/test.scss.map'].content)).toEqual(map);
   });
 
-  it('should support precision option', () => {
-    const task = sass({ precision: 3 });
+  it('should support precision option', async () => {
+    const { run, files } = await setup({
+      'precision.scss': fromFixture('fixtures/precision.scss'),
+    });
 
-    const file = {
-      filename: 'precision.scss',
-      content: fs.readFileSync(path.join(__dirname, 'fixtures/precision.scss'), 'utf8'),
-    };
-
-    return task([file])
-      .then(([result]) => {
-        expect(result.content).toMatch(/1\.343/);
+    await run(async ({ [taskPath]: sass }) => {
+      await sass({
+        pattern: '*.scss',
+        target: 'dist',
+        options: { precision: 3 },
       });
+    });
+
+    expect(files['dist/precision.scss'].content).toMatch('1.343');
   });
 });
