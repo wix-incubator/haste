f21e9f25ae476d2e3a15e9a8de7ac336a8293aee
Ran Yitzhaki <ranyitzhaki@gmail.com>
2017-12-03 01:27:20 +0200

    haste-core: resolution of tasks supports relative paths and camelCased methods

--- packages/haste-core/src/utils.js
+++ packages/haste-core/src/utils.js
@@ -7,16 +7,18 @@ const standardizeTaskName = (name) => {
   return name.replace(HASTE_TASK_PREFIX_REGEX, 'haste-task-');
 };
 
+const camelCaseToDash = str => str.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+
+const isPath = str => path.basename(str) !== str;
+
 module.exports.resolveTaskName = (taskName, runContext) => {
   if (path.isAbsolute(taskName)) {
     return taskName;
   }
 
-  return resolveFrom(runContext, standardizeTaskName(taskName));
+  taskName = isPath(taskName) ? taskName : standardizeTaskName(camelCaseToDash(taskName));
+
+  return resolveFrom(runContext, taskName);
 };
 
 module.exports.flatten = list => list.reduce((sub, elm) => sub.concat(elm), []);
-
-module.exports.camelCaseToDash = str => str.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
-
-module.exports.isPath = str => path.basename(str) !== str;
