3a6d463ea01cea10c6f2ebe00658050363f9e261
Ronen Amiel <ronen.amiel@gmail.com>
2017-12-10 00:25:57 +0200

    haste-core: use retry-promise for idle tests instead of relying on timeouts

--- packages/haste-core/test/fixtures/idle.js
+++ packages/haste-core/test/fixtures/idle.js
@@ -1,9 +1,5 @@
-const sleep = time => new Promise(resolve => setTimeout(resolve, time));
-
 module.exports = async () => {
   console.log('idle');
-
-  await sleep(100); // prevent flakiness and having to use retry-promise
 
   return {
     log: async () => {
