5cd1894d39f88a3d2f71f42db4093431d87311f6
Ronen Amiel <ronen.amiel@gmail.com>
2017-12-09 19:51:37 +0200

    haste-core: better error handling for resolving a task path from it's name

--- packages/haste-core/src/utils.js
+++ packages/haste-core/src/utils.js
@@ -4,7 +4,9 @@ const resolveFrom = require('resolve-from');
 const HASTE_TASK_PREFIX_REGEX = /^(?!@|[^/]+\/|haste-task-)/;
 
 const standardizeTaskName = (name) => {
-  return name.replace(HASTE_TASK_PREFIX_REGEX, 'haste-task-');
+  return camelCaseToDash(
+    name.replace(HASTE_TASK_PREFIX_REGEX, 'haste-task-'),
+  );
 };
 
 const camelCaseToDash = str => str.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
@@ -13,12 +15,20 @@ const isPath = str => path.basename(str) !== str;
 
 module.exports.resolveTaskName = (taskName, runContext) => {
   if (path.isAbsolute(taskName)) {
-    return taskName;
+    try {
+      return require.resolve(taskName);
+    } catch (error) {
+      throw new Error(`Cannot resolve task ${taskName}`);
+    }
   }
 
-  taskName = isPath(taskName) ? taskName : standardizeTaskName(camelCaseToDash(taskName));
+  const resolvedTaskName = !isPath(taskName) ? standardizeTaskName(taskName) : taskName;
 
-  return resolveFrom(runContext, taskName);
+  try {
+    return resolveFrom(runContext, resolvedTaskName);
+  } catch (error) {
+    throw new Error(`Cannot resolve task ${taskName} in ${runContext}`);
+  }
 };
 
 module.exports.flatten = list => list.reduce((sub, elm) => sub.concat(elm), []);
