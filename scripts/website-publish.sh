60301bffe046b4207188d3112434f37c2a2f7641
Ran Yitzhaki <ranyitzhaki@gmail.com>
2018-01-06 16:42:37 +0200

    fix: remove specific git user from the website:publish script

--- scripts/website-publish.sh
+++ scripts/website-publish.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-GIT_USER=ranyitz CURRENT_BRANCH=master USE_SSH=true npm run publish-gh-pages --prefix website
+CURRENT_BRANCH=master USE_SSH=true npm run publish-gh-pages --prefix website
