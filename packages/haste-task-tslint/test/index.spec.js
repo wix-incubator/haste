97b4f8d3873a86c5cad127985958c6ffaeeb3816
Lior Belinsky <liorbe@LiorBe-MBP15-2.local>
2018-04-18 14:46:42 +0300

    organize the tests

--- packages/haste-task-tslint/test/index.spec.js
+++ packages/haste-task-tslint/test/index.spec.js
@@ -11,82 +11,125 @@ const pathToInvalidTsconfigFile = require.resolve('./fixtures/invalidTsconfig.js
 describe('haste-tslint', () => {
   let test;
 
-  it('should resolve for valid files', async () => {
-    test = await setup();
-
-    await test.run(async ({ [taskPath]: tslint }) => {
-      await tslint({
-        tsconfigFilePath: pathToValidTsconfigFile,
-        tslintFilePath: pathToTslintFile
+  describe('linter', () => {
+    it('should resolve for valid files', async () => {
+      test = await setup();
+  
+      await test.run(async ({ [taskPath]: tslint }) => {
+        await tslint({
+          tsconfigFilePath: pathToValidTsconfigFile,
+          tslintFilePath: pathToTslintFile
+        });
       });
     });
+  
+    it('should reject for invalid files', async () => {
+      expect.assertions(1);
+  
+      test = await setup();
+  
+      await test.run(async ({ [taskPath]: tslint }) => {
+        try {
+          await tslint({
+            tsconfigFilePath: pathToInvalidTsconfigFile,
+            tslintFilePath: pathToTslintFile,
+          });
+        } catch (error) {
+          expect(error.message).toMatch('Calls to \'console.error\' are not allowed');
+        }
+      });
+    });  
+    
+    it('should pass configuration to the linter', async () => {
+      expect.assertions(1);
+  
+      test = await setup();
+  
+      await test.run(async ({ [taskPath]: tslint }) => {
+        try {
+          await tslint({
+            tsconfigFilePath: pathToInvalidTsconfigFile,
+            tslintFilePath: pathToTslintFile,
+            options: { formatter: 'verbose' },
+          });
+        } catch (error) {
+          expect(error.message).toMatch('ERROR: (no-console)');
+        }
+      });
+    });  
   });
 
-  it('should reject for invalid files', async () => {
-    expect.assertions(1);
+  describe('tslintFilePath', () => {
+    const errorMessage = 'Could not find config file';
 
-    test = await setup();
-
-    await test.run(async ({ [taskPath]: tslint }) => {
-      try {
-        await tslint({
-          tsconfigFilePath: pathToInvalidTsconfigFile,
-          tslintFilePath: pathToTslintFile,
-        });
-      } catch (error) {
-        expect(error.message).toMatch('Calls to \'console.error\' are not allowed');
-      }
+    it('should reject if a tslint.json could not be found', async () => {
+      expect.assertions(1);
+  
+      test = await setup();
+  
+      await test.run(async ({ [taskPath]: tslint }) => {
+        try {
+          await tslint({
+            tsconfigFilePath: pathToValidTsconfigFile,
+            tslintFilePath: 'no-file',
+          });
+        } catch (error) {
+          expect(error.message).toMatch(errorMessage);
+        }
+      });
     });
+  
+    it('should reject if a tslint.json was not provided', async () => {
+      expect.assertions(1);
+  
+      test = await setup();
+  
+      await test.run(async ({ [taskPath]: tslint }) => {
+        try {
+          await tslint({
+            tsconfigFilePath: pathToValidTsconfigFile,
+          });
+        } catch (error) {
+          expect(error.message).toMatch(errorMessage);
+        }
+      });
+    });  
   });
-
-  it('should reject if a tslint.json could not be found', async () => {
-    expect.assertions(1);
-
-    test = await setup();
-
-    await test.run(async ({ [taskPath]: tslint }) => {
-      try {
-        await tslint({
-          tsconfigFilePath: pathToValidTsconfigFile,
-          tslintFilePath: 'no-file',
-        });
-      } catch (error) {
-        expect(error.message).toMatch('Could not find config file');
-      }
+  
+  describe('tsConfigFilePath', () => {
+    const errorMessage = 'The specified path does not exist';
+    
+    it('should reject if a tsconfig.json could not be found', async () => {
+      expect.assertions(1);
+  
+      test = await setup();
+  
+      await test.run(async ({ [taskPath]: tslint }) => {
+        try {
+          await tslint({
+            tsconfigFilePath: 'no-file',
+            tslintFilePath: pathToTslintFile,
+          });
+        } catch (error) {
+          expect(error.message).toMatch(errorMessage);
+        }
+      });
     });
-  });
-
-  it('should reject if a tslint.json was not provided', async () => {
-    expect.assertions(1);
-
-    test = await setup();
-
-    await test.run(async ({ [taskPath]: tslint }) => {
-      try {
-        await tslint({
-          tsconfigFilePath: pathToValidTsconfigFile,
-        });
-      } catch (error) {
-        expect(error.message).toMatch('Could not find config file');
-      }
-    });
-  });
-
-  it('should pass configuration to the linter', async () => {
-    expect.assertions(1);
-
-    test = await setup();
-
-    await test.run(async ({ [taskPath]: tslint }) => {
-      try {
-        await tslint({
-          tsconfigFilePath: pathToInvalidTsconfigFile,
-          tslintFilePath: pathToTslintFile,
-          options: { formatter: 'verbose' },
-        });
-      } catch (error) {
-        expect(error.message).toMatch('ERROR: (no-console)');
-      }
-    });
+  
+    it('should reject if a tsconfig.json was not provided', async () => {
+      expect.assertions(1);
+  
+      test = await setup();
+  
+      await test.run(async ({ [taskPath]: tslint }) => {
+        try {
+          await tslint({
+            tslintFilePath: pathToTslintFile,
+          });
+        } catch (error) {
+          expect(error.message).toContain(errorMessage);
+        }
+      });
+    });  
   });
 });
